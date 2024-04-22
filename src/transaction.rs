ff808b66008e28f946fdfa39d29fb0be5f957d4f
ohmycloud <ohmycloudy@gmail.com>
2024-04-22 15:02:25 +0800

    解析交易数据

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,48 +1,46 @@
-use std::str::FromStr;
-use nom::branch::alt;
-use nom::bytes::complete::{tag, take_until};
-use nom::IResult;
+#![warn(dead_code)]
+
 use nom::bytes::complete::take;
-use nom::sequence::tuple;
+use nom::bytes::complete::{tag, take_until};
 use nom::character::complete;
+use nom::character::complete::{alpha1, newline, space1};
+use nom::error::{Error, ErrorKind};
+use nom::multi::{many0, many1};
 use nom::number::complete::double;
+use nom::sequence::tuple;
+use nom::IResult;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+struct Transaction {
+    kind: TransactionKind,
+    date: Date,
+    description: String,
+    amount: f64,
+}
 
 #[derive(Debug, Eq, PartialEq)]
 enum TransactionKind {
-    CREDIT, // 信用卡
-    DEBIT,  // 银行卡
-}
-
-impl FromStr for TransactionKind {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let kind = match s {
-            "CREDIT" => TransactionKind::CREDIT,
-            "DEBIT" => TransactionKind::DEBIT,
-            _ => return Err(format!("{} can't be converted", s))
-        };
-        Ok(kind)
-    }
+    CREDIT,  // 信用卡
+    DEBIT,   // 银行卡
+    Unknown, // 未知
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct Date {
     month: u8,
     day: u8,
-    year: u16
+    year: u16,
 }
 
 struct Amount {
-    value_times100: u8
+    value_times100: u8,
 }
 
-fn parse_credit(input: &str) -> IResult<&str, &str> {
-    Ok(("", "CREDIT"))
-}
-
-fn parse_debit(input: &str) -> IResult<&str, &str> {
-    Ok(("", "DEBIT"))
+fn parse_month(input: &str) -> IResult<&str, u8> {
+    let (input, month) = take(2usize)(input)?;
+    let res = complete::u8(month)?;
+    Ok((input, res.1))
 }
 
 fn parse_day(input: &str) -> IResult<&str, u8> {
@@ -52,27 +50,27 @@ fn parse_day(input: &str) -> IResult<&str, u8> {
     Ok((input, res.1))
 }
 
-fn parse_month(input: &str) -> IResult<&str, u8> {
-    let (input, month) = take(2usize)(input)?;
-    let res = complete::u8(month)?;
-    Ok((input, res.1))
-}
-
 fn parse_year(input: &str) -> IResult<&str, u16> {
     let (input, year) = take(4usize)(input)?;
     let res = complete::u16(year)?;
     Ok((input, res.1))
 }
 
-fn parse_mmddyyyy(input: &str) -> IResult<&str, Date> {
-    let mut parser = tuple((parse_day, parse_month, parse_year));
-    let (input, (day, month, year)) = parser(input)?;
-    Ok((input, Date {day, month, year}))
+fn parse_date(input: &str) -> IResult<&str, Date> {
+    let mut parser = tuple((parse_month, parse_day, parse_year));
+    let (input, (month, day, year)) = parser(input)?;
+    Ok((input, Date { month, day, year }))
 }
 
-fn parse_kind(input: &str) -> IResult<&str, TransactionKind > {
-    let (input, kind) = alt((parse_credit, parse_debit))(input)?;
-    Ok((input, kind.parse::<TransactionKind>().unwrap()))
+fn parse_kind(input: &str) -> IResult<&str, TransactionKind> {
+    let (input, kind) = alpha1(input)?;
+    let kind = match kind {
+        "CREDIT" => TransactionKind::CREDIT,
+        "DEBIT" => TransactionKind::DEBIT,
+        _ => TransactionKind::Unknown,
+    };
+
+    Ok((input, kind))
 }
 
 fn parse_description(input: &str) -> IResult<&str, &str> {
@@ -86,15 +84,51 @@ fn parse_amount(input: &str) -> IResult<&str, f64> {
     Ok((input, amount.1))
 }
 
+fn parse_transaction(input: &str) -> IResult<&str, Transaction> {
+    let mut parser = tuple((
+        many0(newline),
+        space1,
+        parse_kind,
+        space1,
+        parse_date,
+        space1,
+        parse_description,
+        parse_amount,
+        many0(newline),
+    ));
+    let (input, (_, _, kind, _, date, _, description, amount, _)) = parser(input)?;
+
+    Ok((
+        input,
+        Transaction {
+            kind,
+            date,
+            description: description.into(),
+            amount,
+        },
+    ))
+}
+
+fn parse_transactions(input: &str) -> IResult<&str, Vec<Transaction>> {
+    let mut parser = many1(parse_transaction);
+    let (input, transactions) = parser(input)?;
+    Ok((input, transactions))
+}
+
 fn main() {
     let input = r#"
     CREDIT    04062020    PayPal transfer    $4.99
     CREDIT    04032020    Payroll            $69.73
     DEBIT     04022020    ACH transfer       $38.25
     DEBIT     03242020    IRS tax kind       $52249.98
-    "#.trim_start();
+    "#
+    .trim_end();
 
-    println!("{}", input);
+    if let Ok((_, transactions)) = parse_transactions(input) {
+        println!("{:#?}", transactions);
+    } else {
+        print!("parse failed with: {:?}", input);
+    }
 }
 
 #[test]
@@ -104,19 +138,19 @@ fn test_trans_kind() {
 }
 
 #[test]
-fn test_day() {
+fn test_date() {
     let input = "04062020";
-    assert_eq!(parse_day(input), Ok(("", 4)));
-}
-
-#[test]
-fn test_mmddyyyy() {
-    let input = "04062020";
-    assert_eq!(parse_mmddyyyy(input), Ok(("", Date {
-        day: 4,
-        month: 6,
-        year: 2020,
-    })));
+    assert_eq!(
+        parse_date(input),
+        Ok((
+            "",
+            Date {
+                day: 6,
+                month: 4,
+                year: 2020,
+            }
+        ))
+    );
 }
 
 #[test]
@@ -129,4 +163,25 @@ fn test_description() {
 fn test_amount() {
     let input = "$4.99";
     assert_eq!(parse_amount(input), Ok(("", 4.99)));
+}
+
+#[test]
+fn test_transaction() {
+    let input = "    CREDIT    04062020    PayPal transfer    $4.99";
+    assert_eq!(
+        parse_transaction(input),
+        Ok((
+            "",
+            Transaction {
+                kind: TransactionKind::CREDIT,
+                date: Date {
+                    month: 4,
+                    day: 6,
+                    year: 2020
+                },
+                description: "PayPal transfer".into(),
+                amount: 4.99,
+            }
+        ))
+    );
 }
