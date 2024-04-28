218a4b87218ed0317653242788bdca5b191ae20d
ohmycloud <ohmycloudy@gmail.com>
2024-04-28 18:42:47 +0800

    parse binary string

---
+++ src/inclusion.rs
@@ -0,0 +1,63 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Inclusion {
+    #[arg(long)]
+    hex: String
+}
+
+impl Inclusion {
+
+    fn to_binary(c: char) -> &'static str {
+        match c {
+            '0' => "0000",
+            '1' => "0001",
+            '2' => "0010",
+            '3' => "0011",
+            '4' => "0100",
+            '5' => "0101",
+            '6' => "0110",
+            '7' => "0111",
+            '8' => "1000",
+            '9' => "1001",
+            'A' | 'a' => "1010",
+            'B' | 'b' => "1011",
+            'C' | 'c' => "1100",
+            'D' | 'd' => "1101",
+            'E' | 'e' => "1110",
+            'F' | 'f' => "1111",
+            _ => "",
+        }
+    }
+
+    fn binary(&self) -> String {
+        self.hex[2..].chars().map(Self::to_binary).collect()
+    }
+    fn hex_to_binary(&self) -> String {
+        let binary_string = if let Ok(hex_value) = u16::from_str_radix(self.hex.trim_start_matches("0x"), 16) {
+            let binary_string = format!("{:016b}", hex_value);
+            binary_string
+        } else {
+            "".into()
+        };
+        binary_string
+    }
+
+}
+
+fn main() {
+    let inclusion = Inclusion::parse();
+    println!("{:?}", inclusion.binary());
+}
+
+#[test]
+fn test_inclusion() {
+    let inclusion = Inclusion { hex: "0xFFFFFFFFFFFE".into() };
+    assert_eq!(inclusion.binary(), "111111111111111111111111111111111111111111111110");
+
+    let inclusion = Inclusion { hex: "0xffff8".into() };
+    assert_eq!(inclusion.binary(), "11111111111111111000");
+
+    let inclusion = Inclusion { hex: "0x1FD3C00800000000".into() };
+    assert_eq!(inclusion.binary(), "1111111010011110000000000100000000000000000000000000000000000");
+}
--- src/with_nom/iot.rs
+++ src/with_nom/iot.rs
@@ -60,31 +60,6 @@ fn parse_json(input: &str) -> IResult<&str, &str> {
     Ok((input, json))
 }
 
-fn convert_to_binary_from_hex(hex: &str) -> String {
-    hex[2..].chars().map(to_binary).collect()
-}
-
-fn to_binary(c: char) -> &'static str {
-    match c {
-        '0' => "0000",
-        '1' => "0001",
-        '2' => "0010",
-        '3' => "0011",
-        '4' => "0100",
-        '5' => "0101",
-        '6' => "0110",
-        '7' => "0111",
-        '8' => "1000",
-        '9' => "1001",
-        'A' => "1010",
-        'B' => "1011",
-        'C' => "1100",
-        'D' => "1101",
-        'E' => "1110",
-        'F' => "1111",
-        _ => "",
-    }
-}
 
 #[test]
 fn test_server_time() {
