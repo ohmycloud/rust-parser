bb0af786649e8ab6e41d0f054a278b510cc2f73d
ohmycloud <ohmycloudy@gmail.com>
2024-05-06 23:44:40 +0800

    add input

--- src/with_nom/section.rs
+++ src/with_nom/section.rs
@@ -1,1 +1,21 @@
 fn parse_section() {}
+
+#[test]
+fn test_section() {
+    let input = r#"Here's some unimportant text.
+    =begin code
+    This code block is what we're after.
+    We'll use 'ff' to get it.
+    =end code
+    More unimportant text.
+    =begin code
+    I want this line.
+    and this line as well.
+    HaHa.
+    =end code
+    More unimport text.
+    =begin code
+    Let's to go home.
+    =end code"#;
+
+}
