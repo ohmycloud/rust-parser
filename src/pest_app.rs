9b3d1d37b01b121456a8681ef552829aebc202aa
ohmycloud <ohmycloudy@gmail.com>
2025-11-19 21:56:12 +0800

    parse transaction with pest

---
+++ src/pest_app.rs
@@ -0,0 +1,23 @@
+use pest::Parser;
+
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar/transaction.pest"]
+pub struct TransactionParser;
+
+fn main() {
+    let transaction: &str = r###"CREDIT    04062020    PayPal transfer    $4.99
+        CREDIT    04032020    Payroll            $69.73
+        DEBIT     04022020    ACH transfer       $38.25
+        DEBIT     03242020    IRS tax payment    $52249.98
+    "###;
+
+    println!("{:?}", transaction);
+
+    let p = TransactionParser::parse(Rule::transactions, transaction)
+        .expect("unsuccessful parse")
+        .next()
+        .unwrap();
+    println!("{:?}", p);
+}
