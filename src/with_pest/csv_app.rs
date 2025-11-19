c9e0b6978bcbcad833a2ffa8aab652b05321a907
ohmycloud <ohmycloudy@gmail.com>
2025-11-19 22:14:02 +0800

    parse csv and ini with pest

---
+++ src/with_pest/csv_app.rs
@@ -0,0 +1,42 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+use pest::Parser;
+use std::fs;
+
+#[derive(Parser)]
+#[grammar = "grammar/csv.pest"]
+pub struct CSVParser;
+
+fn main() {
+    let mut field_sum: f64 = 0.0;
+    let mut record_count: u64 = 0;
+
+    let unparsed_file = fs::read_to_string("src/grammar/number.csv").expect("can't read file");
+    let file = CSVParser::parse(Rule::file, &unparsed_file)
+        .expect("unsuccessful parse") // unwrap the parse result
+        .next()
+        .unwrap(); // get and unwrap the `file` rule; never fails
+
+    for record in file.into_inner() {
+        match record.as_rule() {
+            Rule::record => {
+                record_count += 1;
+
+                for field in record.into_inner() {
+                    field_sum += field.as_str().parse::<f64>().unwrap();
+                }
+            }
+            Rule::EOI => (),
+            _ => unreachable!(),
+        }
+    }
+    println!("Sum of fields: {}", field_sum);
+    println!("Number of records: {}", record_count);
+
+    // let successful_parse = CSVParser::parse(Rule::field, "-273.15");
+    // println!("{:?}", successful_parse);
+
+    // let unsuccessful_parse = CSVParser::parse(Rule::field, "not a number");
+    // println!("{:?}", unsuccessful_parse);
+}
